c9737a74cd13bb35e11c8e08e735e75415af634e
Chase Jensen <soccerchase@gmail.com>
2018-06-19 01:36:13 -0600

    Commit before using new Emitter node module

--- nodeJS/customer_node_event_emitter/emitter.js
+++ nodeJS/customer_node_event_emitter/emitter.js
@@ -5,13 +5,27 @@
 // 2) Custom Events - Provided by the Javascript core and handled by the Event Emitter
 //    which this Emitter is a slightly simpler version.
 // In a nutshell, this Emitter essentially allow us to say an event has happened and
-// to it. This could be built in many ways including ES6, this version will simply
+// respond to it. This could be built in many ways including ES6, this version will simply
 // use the standard first class function.
 
 function Emitter() {
   this.events = {};
 }
 
+// Listen for custom event, such as fileWasSaved(), receivedFromInternet(), whatever
+// custom events you want.
 Emitter.prototype.on = function(type, listener) {
-
+  this.events[type] = this.events[type] || [];
+  this.events[type].push(listener);
 }
+
+
+Emitter.prototype.emit = function(type) {
+  if (this.events[type]){
+    this.events[type].forEach(function(listener) {
+      listener();
+    });
+  }
+}
+
+module.exports = Emitter;
