aa8f3bc072241200d9d83a9b400d7aee0dd3d86c
Chase Jensen <soccerchase@gmail.com>
2018-06-19 01:48:12 -0600

    Customer file system event listener..

--- nodeJS/customer_node_event_emitter/app.js
+++ nodeJS/customer_node_event_emitter/app.js
@@ -11,6 +11,17 @@ emtr.on('greet', function(){
   console.log('A greeting occurred!');
 });
 
+// If would imagine an object could/should be used so I can pass paramaters to my emit function?
+emtr.on('fsChange', function(){
+  // pretend name was passed in
+  // Nodemon!
+  name = 'index.js'
+  console.log(name + ' ' + 'was changed! Please re-render the browser accordingly.')
+})
+
 // console.log is the event we are saying is the event, call the emit event when emitted.
 console.log('Hello!');
 emtr.emit('greet');
+
+console.log('I Changed a file!');
+emtr.emit('fsChange');
