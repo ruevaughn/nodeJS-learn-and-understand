1377562f56a6dba25f8d6302d2989651bfde65fc
Chase Jensen <soccerchase@gmail.com>
2018-06-19 02:02:00 -0600

    Refactor to use variables rather than magic strings.

--- nodeJS/customer_node_event_emitter/app.js
+++ nodeJS/customer_node_event_emitter/app.js
@@ -1,18 +1,20 @@
+// Most Modules are built on top of the event emitter.
 // Use actual Node Emitter now - works exactly the same though much much much more extensive.
 var Emitter = require('events');
+var eventConfig = require('./config').events;
 
 var emtr = new Emitter();
 
 // This will run whenever the 'greet' event is emitted.
-emtr.on('greet', function(){
+emtr.on(eventConfig.GREET, function(){
   console.log('Somewhere, someone said hello.');
 });
 
-emtr.on('greet', function(){
+emtr.on(eventConfig.GREET, function(){
   console.log('A greeting occurred!');
 });
 
-emtr.on('fsChange', function(){
+emtr.on(eventConfig.FILESAVED, function(){
   // pretend name was passed in
   name = 'index.js'
   console.log(name + ' ' + 'was changed! Please re-render the browser accordingly.')
