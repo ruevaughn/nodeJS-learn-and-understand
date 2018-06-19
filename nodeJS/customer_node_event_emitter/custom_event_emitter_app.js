797613a0ee2c34e72eb40041c74893091ed0f34a
Chase Jensen <soccerchase@gmail.com>
2018-06-19 01:56:40 -0600

    Change to actual Node Event Emitter

--- nodeJS/customer_node_event_emitter/custom_event_emitter_app.js
+++ nodeJS/customer_node_event_emitter/custom_event_emitter_app.js
@@ -1,5 +1,4 @@
-// Use actual Node Emitter now - works exactly the same though much much much more extensive.
-var Emitter = require('events');
+var Emitter = require('./emitter');
 
 var emtr = new Emitter();
 
@@ -12,12 +11,18 @@ emtr.on('greet', function(){
   console.log('A greeting occurred!');
 });
 
+// If would imagine an object could/should be used so I can pass paramaters to my emit function?
+// Yup, after looking at the Node actual event emitter, events is an object so properties
+// can indeed be passed in. I guess that makes sense as I can't see any other way this
+// could be viable if not.
 emtr.on('fsChange', function(){
   // pretend name was passed in
+  // Nodemon!
   name = 'index.js'
   console.log(name + ' ' + 'was changed! Please re-render the browser accordingly.')
 })
 
+// console.log is the event we are saying is the event, call the emit event when emitted.
 console.log('Hello!');
 emtr.emit('greet');
 
